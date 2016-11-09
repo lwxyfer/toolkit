5953d6e070bc0f426e74e2c8fd8fd42275c638a4
lwxyfer <lwxyfer@gmail.com>
2016-11-09 19:13:30 +0800

    问君能有几多愁

--- slider/slider.js
+++ slider/slider.js
@@ -1,61 +1,89 @@
 'use strict';
 
+/**
+ * Slider --
+ * Author: LWXYFER
+ * Following amazeui rules
+ */
+
 var Slider = function(element, options) {
 
-  console.log(element)
-  this.$slider = $(element);
-  console.log(this.$slider)
+  this.$element = $(element);
+
+  // simplify html structure
+  this.$element.html('<div class="slider-con">' + this.$element.html() + '</div>');
+
+  // users has other names
+  this.$element.addClass('slider');
+
   this.$con = $('.slider-con');
+  this.$child = this.$con.children();
 
   this.options = $.extend({}, Slider.DEFAULTS, options);
 
-  this.$width = this.$slider.width();
-  this.$number = this.$con.children();
-  this.$length = this.$number.length;
-  this.$allWidth = this.$width*this.$number.length;
-
-  this.$fullWidth = this.$width*(this.$number.length+1);
-  this.$numWidth = this.$width*(this.$number.length-1);
-
-
-  this.$slider.css('height', this.options.height);
-  this.$slider.css('width', this.options.width);
-
-  this.$con.css('width', (this.$fullWidth).toString() );
-
-  this.$con.append(this.$con.children().eq(0).clone());
-  this.$con.prepend(this.$con.children().eq(this.$length-1).clone());
-
-  this.$con.css('left', -this.$width)
-
+  // Reference: debounce & throttle
   this.status = true;
 
-  if(this.options.keyboard) {
-    this.keyboard();
-  }
-  if(this.options.autoplay) {
-    this.autoplay();
-  }
-  if(this.options.arrow) {
-    this.arrow();
-  }
-  if(this.options.dots) {
-    this.dots();
-  }
+  this.init();
+  this.resize();
 }
 
 Slider.DEFAULTS = {
   width: '100%',
   height: 'auto',
   speed: 200,
-  loop: true,
+  infinite: true,
   autoplay: false,
   autoplaySpeed: 1000,
   arrow: true,
-  arrowPre:null,
+  arrowPre: null,
   arrowNext: null,
   dots: true,
   keyboard: true,
+  callback: null, // after every move
+}
+
+Slider.prototype.init = function() {
+
+  var $element = this.$element;
+  var $child = this.$child;
+  var $con = this.$con;
+  var options = this.options;
+
+  $element.css('width', options.width);
+  this.width = $element.width(); // if use percentage, need get real width
+  this.length = $child.length;
+
+  console.log('width', this.width)
+
+  this.$allWidth = this.width*$child.length;
+  this.$fullWidth = this.width*($child.length+1);
+  this.$numWidth = this.width*($child.length-1);
+
+  // cant use percentage
+  $child.css('width', this.width)
+  $element.css('height', options.height);
+
+  $con.css('width', (this.$fullWidth).toString() );
+
+  // infinitescroll
+  $con.append($con.children().eq(0).clone());
+  $con.prepend($con.children().eq(this.length-1).clone());
+
+  $con.css('left', -this.width);
+
+  if(options.keyboard) {
+    this.keyboard();
+  }
+  if(options.autoplay) {
+    this.autoplay();
+  }
+  if(options.arrow) {
+    this.arrow();
+  }
+  if(options.dots) {
+    this.dots();
+  }
 }
 
 Slider.prototype.destory = function() {
@@ -70,32 +98,32 @@ Slider.prototype.destory = function() {
 Slider.prototype.move = function(distance) {
   var _this = this;
 
-  // 不加无线滚动
-  // var ml = Number(this.$con.css('left').slice(0,-2));
-  // console.log('margin: %s, distance: %s',ml, distance);
-  // if( (ml === 0 && distance < 0) || (ml === -this.$numWidth && distance >0))  {
-  //   return false;
-  // }
+  // not infinite scroll
+  if( !_this.infinite ) {
+    if(_this.index === _this.length) {
+      return false;
+    }
+  }
 
   if( _this.status ) {
     (function() {
       _this.status = !_this.status;
+
       _this.$con.animate({
         left: '-=' + distance,
-      }, _this.options.speed, () => {
+      }, _this.options.speed, function() {
+
         console.log('ok');
         _this.status = !_this.status;
 
-        if(_this.index() === _this.$length) {
-          _this.$con.css('left', -_this.$width);
+        if(_this.index() === _this.length) {
+          _this.$con.css('left', -_this.width);
           console.log('change')
         }
         if(_this.index() === -1) {
           _this.$con.css('left', -_this.$allWidth);
           console.log('change')
         }
-
-        $('.dot').removeClass('active')
 
         _this.dotActive();
       })
@@ -106,12 +134,12 @@ Slider.prototype.move = function(distance) {
 
 Slider.prototype.next = function() {
   var _this = this;
-  return this.move(_this.$width);
+  return this.move(_this.width);
 }
 
 Slider.prototype.pre = function() {
   var _this = this;
-  this.move(-_this.$width);
+  this.move(-_this.width);
 }
 
 Slider.prototype.keyboard = function() {
@@ -126,20 +154,19 @@ Slider.prototype.keyboard = function() {
   })
 }
 
-Slider.prototype.isOver = function() {
-  var ml = this.$con.css('left');
-  console.log(ml);
-  if( ml > 0 || -ml > this.$allWidth - this.$width) {
-    return true;
-  }
+Slider.prototype.index = function() {
+  // var _this = this;
+  var ml = Number(this.$con.css('left').slice(0,-2));
+  var w = this.width;
+  var i = Math.abs(-ml/w) - 1;
+  return i;
 }
 
 Slider.prototype.autoplay = function() {
   var _this = this;
   var timer = setInterval(function() {
     var x = _this.next();
-    console.log( Date.now())
-    console.log(x)
+
     if(!x) {
       clearTimeout(timer)
     }
@@ -154,28 +181,24 @@ Slider.prototype.hoverStop = function() {
 Slider.prototype.dots = function() {
   var dotsTem = '<div class="dots">';
   var dotTem = '<span class="dot"></span>';
-  for(var i=0; i< this.$length; i++) {
+
+  for(var i=0; i< this.length; i++) {
     dotsTem += dotTem;
   }
-  dotsTem += '</div>'
-  this.$slider.append(dotsTem);
+
+  dotsTem += '</div>';
+
+  this.$element.append(dotsTem);
   this.dotActive();
   this.dotMove()
 }
 
 Slider.prototype.dotActive = function() {
-  var _this = this;
-  var i = _this.index();
+  var i = this.index();
   console.log('current index', i);
-  $('.dots .dot ').eq(i).addClass('active');
-}
 
-Slider.prototype.index = function() {
-  var _this = this;
-  var ml = Number(_this.$con.css('left').slice(0,-2));
-  var w = _this.$width;
-  var i = Math.abs(-ml/w) - 1;
-  return i;
+  $('.dots .dot').removeClass('active');
+  $('.dots .dot ').eq(i).addClass('active');
 }
 
 Slider.prototype.dotMove = function() {
@@ -184,23 +207,36 @@ Slider.prototype.dotMove = function() {
   $('.dot').on('click', function() {
     var self = this;
     var i = _this.index();
-    console.log('dot distance', ($(self).index() - i) * _this.$width)
-    _this.move( ($(self).index() - i) * _this.$width )
+
+    console.log('dot distance', ($(self).index() - i) * _this.width);
+
+    _this.move( ($(self).index() - i) * _this.width )
   })
 
 }
 
 Slider.prototype.arrow = function() {
   var _this = this;
+
   var arrowBf = '<span class="arrow-bf"><</span>';
   var arrowAf = '<span class="arrow-af">></span>';
-  this.$slider.append(arrowBf);
-  this.$slider.append(arrowAf);
+
+  this.$element.append(arrowBf);
+  this.$element.append(arrowAf);
+
   $('.arrow-af').on('click', function() {
     _this.next();
   })
   $('.arrow-bf').on('click', function() {
     _this.pre();
+  })
+}
+
+Slider.prototype.resize = function() {
+  var _this = this;
+  $(window).on('resize', function() {
+    console.log('11')
+    _this.init();
   })
 }
 
@@ -222,8 +258,9 @@ $.fn.slider = function( options) {
   var slider = new Slider(this, options);
   return slider;
 }
+
 var aa = $('.slider').slider({
-  width: 800,
+  width: '100%',
   height: 500,
   // autoplay: true,
 })
